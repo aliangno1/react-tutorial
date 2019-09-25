17a8179f42210c1db12387e55088166353b960ad
litex tech 05 <1123084484@qq.com>
2019-09-25 18:15:14 +0800

    redux-basic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-// import App from './Api'
 import './index.css'
+// import App from './App'
+// import App from './Api'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+let store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
