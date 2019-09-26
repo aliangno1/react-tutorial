82b0546abc1c1af9967755210e8c3b5148510835
litex tech 05 <1123084484@qq.com>
2019-09-26 19:12:35 +0800

    react-router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
+import App from './AppRouter'
 
 ReactDOM.render(<App />, document.getElementById('root'))
